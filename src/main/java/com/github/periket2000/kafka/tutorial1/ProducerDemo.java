8e3be97100aae7a99db5de4b9383632373c24bf6
Marco Albero <periket2000@gmail.com>
2019-08-28 00:45:34 +0200

    Initial commit

---
+++ src/main/java/com/github/periket2000/kafka/tutorial1/ProducerDemo.java
@@ -0,0 +1,26 @@
+package com.github.periket2000.kafka.tutorial1;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Date;
+import java.util.Properties;
+
+public class ProducerDemo {
+    public static void main(String[] args) {
+        // create properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        // send data
+        producer.send(new ProducerRecord<String, String>("first_topic", (new Date()).toString()));
+        producer.close();
+    }
+}
