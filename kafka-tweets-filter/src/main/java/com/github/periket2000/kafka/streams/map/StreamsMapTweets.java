0ed06696b2f6bceac4634f295147a90717d7af6d
Marco Albero <marco@xtremis.com>
2021-07-14 17:25:01 +0200

    Modifications mainly on ports to run all services on docker

--- kafka-tweets-filter/src/main/java/com/github/periket2000/kafka/streams/map/StreamsMapTweets.java
+++ kafka-tweets-filter/src/main/java/com/github/periket2000/kafka/streams/map/StreamsMapTweets.java
@@ -26,7 +26,7 @@ public class StreamsMapTweets {
     public static void main(String[] args) {
         // create properties
         Properties p = new Properties();
-        p.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        p.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:29092");
         p.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, "stream-map-tweets");
         p.setProperty(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.StringSerde.class.getName());
         p.setProperty(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.StringSerde.class.getName());
