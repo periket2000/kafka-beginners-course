2efaa084599636d85bb1356ae40321948c49abbe
Marco Albero <marco@mybi.es>
2023-04-04 13:37:54 +0200

    Fixed kafka with python prod/cons

--- python/src/consumer.py
+++ python/src/consumer.py
@@ -2,4 +2,6 @@ from kafka import KafkaConsumer
 
 consumer = KafkaConsumer('mi_tema', bootstrap_servers=['localhost:49092'])
 for mensaje in consumer:
-	print(mensaje)
+	print(mensaje.value.decode('utf-8'))
+
+consumer.close()
--- python/src/producer.py
+++ python/src/producer.py
@@ -1,4 +1,8 @@
 from kafka import KafkaProducer
+import random
 
 producer = KafkaProducer(bootstrap_servers=['localhost:49092'])
-producer.send('mi_tema', b'mensaje de prueba')
+for i in range(100):
+	message = "Mensaje de prueba {}".format(str(random.randint(1,100)))
+	producer.send('mi_tema', value=message.encode())
+producer.close()
